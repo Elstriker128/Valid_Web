bff9821f99a30bf59e5f03c30582a4440fe56676
Elstriker128 <89691298+Elstriker128@users.noreply.github.com>
2023-02-14 20:04:37 +0200

    Add files via upload

---
+++ Forma1.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Forma1.aspx.cs" Inherits="ValidWeb.Forma1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            Tournament attendee registration:
+        </p>
+        <asp:ValidationSummary ID="ValidationSummary1" runat="server" ForeColor="Red" />
+        <p>
+            Name: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="Name is essential" ForeColor="Red">*</asp:RequiredFieldValidator>
+        </p>
+        <p>
+            Surname: <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="TextBox2" ErrorMessage="Surname is essential" ForeColor="Red">*</asp:RequiredFieldValidator>
+        </p>
+        <p>
+            School&#39;s name:
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="TextBox3" ErrorMessage="School's name is essential" ForeColor="Red">*</asp:RequiredFieldValidator>
+        </p>
+        <p>
+            Age:<asp:DropDownList ID="DropDownList1" runat="server"> </asp:DropDownList>
+            <asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="DropDownList1" ErrorMessage="Age is essential" ForeColor="Red" MaximumValue="25" MinimumValue="14" Type="Integer">*</asp:RangeValidator>
+        </p>
+        <p>
+            Programming language:<asp:CheckBoxList ID="CheckBoxList1" runat="server" DataSourceID="XmlDataSource1" DataTextField="title" DataValueField="title" ></asp:CheckBoxList>
+            <asp:XmlDataSource ID="XmlDataSource1" runat="server" DataFile="~/App_Data/kalbos.xml"></asp:XmlDataSource>
+        </p>
+        <asp:Button ID="Button1" runat="server" Text="Register!" OnClick="Button1_Click" />
+        <br />
+        <br /> 
+        <asp:Table ID="Table1" runat="server" BorderColor="Black" BorderStyle="Solid" BorderWidth="1px" GridLines="Both">
+        </asp:Table>
+        <br />
+        Participant count:
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+        <p>
+            <asp:Button ID="Button2" runat="server" Text="Delete registrations!" OnClick="Button2_Click" CausesValidation="false"/>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Forma1.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Security;
+
+namespace ValidWeb
+{
+    public partial class Forma1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (DropDownList1.Items.Count == 0)
+            {
+                DropDownList1.Items.Add("-");
+                for (int i = 14; i <= 25; i++)
+                {
+                    DropDownList1.Items.Add(i.ToString());
+                }
+            }
+            FirstData();
+            for (int i = 0; i < Session.Count; i++)
+            {
+                Participant current = (Participant)Session[i.ToString()];
+                TableDataInsert(current);
+            }
+            Label1.Text = (Session.Count).ToString();
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string Name = TextBox1.Text;
+            string Surname = TextBox2.Text;
+            string School = TextBox3.Text;
+            string Age = DropDownList1.SelectedValue;
+            string Languages = string.Empty;
+            foreach(ListItem item in CheckBoxList1.Items)
+            {
+                if(item.Selected)
+                {
+                    if(Languages!=string.Empty)
+                        Languages += "," + item.Text;
+                    else
+                        Languages = item.Text;
+                }
+            }
+
+            Participant current = new Participant(Name,Surname,School,Age,Languages);
+
+            Session[Session.Count.ToString()] = current;
+
+            TableDataInsert(current);
+
+            Label1.Text = (Session.Count).ToString();
+        }
+        private void FirstData()
+        {
+
+            TableRow row = new TableRow();
+
+            TableCell name = new TableCell();
+            name.Text = "Name";
+
+            TableCell surname = new TableCell();
+            surname.Text = "Surname";
+
+            TableCell school = new TableCell();
+            school.Text = "School";
+
+            TableCell age = new TableCell();
+            age.Text = "Age";
+
+            TableCell languages = new TableCell();
+            languages.Text = "Languages";
+
+            row.Cells.Add(name);
+            row.Cells.Add(surname);
+            row.Cells.Add(school);
+            row.Cells.Add(age);
+            row.Cells.Add(languages);
+
+            Table1.Rows.Add(row);
+        }
+        private void TableDataInsert(Participant participant)
+        {
+
+            TableRow row = new TableRow();
+
+            TableCell name = new TableCell();
+            name.Text = participant.Name;
+
+            TableCell surname = new TableCell();
+            surname.Text = participant.Surname;
+
+            TableCell school = new TableCell();
+            school.Text = participant.School;
+
+            TableCell age = new TableCell();
+            age.Text = participant.Age.ToString();
+
+            TableCell languages = new TableCell();
+            languages.Text = participant.Languages;
+
+            row.Cells.Add(name);
+            row.Cells.Add(surname);
+            row.Cells.Add(school);
+            row.Cells.Add(age); 
+            row.Cells.Add(languages);
+
+            Table1.Rows.Add(row);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Table1.Rows.Clear();
+            Label1.Text= string.Empty;
+        }
+    }
+    public class Participant
+    {
+        [Required]
+        public string Name { get; private set; }
+        [Required]
+        public string Surname { get; private set; }
+        [Required]
+        public string School { get; private set; }
+        [Required]
+        public string Age { get; private set; }
+        [Required]
+        public string Languages { get; private set; }
+
+        public Participant(string name, string surname, string school, string age, string languages)
+        {
+            Name = name;
+            Surname = surname;
+            School = school;
+            Age = age;
+            Languages = languages;
+        }
+    }
+}
